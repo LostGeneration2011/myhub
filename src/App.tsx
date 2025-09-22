8c010cbdc5db5861a22c956a2e24cb30e854a6ea
anhdinh <anhdinh0408@gmail.com>
2025-09-22 18:54:00 +0700

    2nd commit: installation charka UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import { Button, HStack } from '@chakra-ui/react'
+
 
 const App = () => {
   return (
-    <div>
-       App
-    </div>
+    <HStack>
+      <Button>Click me</Button>
+    </HStack>
   )
 }
 
